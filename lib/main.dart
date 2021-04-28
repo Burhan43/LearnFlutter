dcf7c0a8717328709b19e4ddab4b7310a7a67043
Burhan Ahmed <burhan43>
2021-04-28 13:20:53 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
@@ -6,6 +5,9 @@ void main() {
   runApp(MaterialApp(
     title: 'Flutter',
     home: HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.blueGrey,
+    ),
   ));
 }
 
@@ -13,55 +15,87 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Beginner App"),
-          backgroundColor: Colors.greenAccent,
-        ),
+      appBar: AppBar(
+        title: Text("Beginner App"),
 
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Align(
-            alignment: Alignment.center,
-            child: Container(
+        // backgroundColor: Colors.greenAccent,
+      ),
 
-              color: Colors.cyanAccent,
+      body: Container(
+        // color: Colors.cyanAccent,
 
-              width: 200,
-              height: 300,
-              // height: MediaQuery.of(context).size.height/2,
-              child: Column(
-                // mainAxisAlignment: MainAxisAlignment.center,
-                // crossAxisAlignment: CrossAxisAlignment.stretch,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                // crossAxisAlignment: CrossAxisAlignment.start,
-                // mainAxisAlignment: MainAxisAlignment.end,
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisAlignment: MainAxisAlignment.start,
-                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.deepOrangeAccent,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.teal,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.amberAccent,
-                  ),
-                ],
-              ),
+        // height: MediaQuery.of(context).size.height/2,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+      ),
+
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            // DrawerHeader(
+            //   child: Align(
+            //     alignment: Alignment.center,
+            //     child: Container(
+            //       child: Text(
+            //         "Drawer Header",
+            //         style: TextStyle(
+            //             color: Colors.white,
+            //             fontWeight: FontWeight.bold,
+            //             fontSize: 30),
+            //       ),
+            //     ),
+            //   ),
+            //   decoration: BoxDecoration(color: Colors.indigoAccent),
+            // ),
+
+            UserAccountsDrawerHeader(
+              accountName: Text("Burhan Ahmad"),
+              accountEmail: Text("burhan.demit00@gmail.com"),
+
+              // currentAccountPicture: Image.network("https://images.unsplash.com/photo-1499540785729-ac6adfa4efbf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGJveXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage("https://images.unsplash.com/photo-1499540785729-ac6adfa4efbf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGJveXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
+              )
+              // currentAccountPicture: Image.network("https://unsplash.com/photos/3XY7BWwGolk"),
             ),
-          ),
-        ));
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("burhan.demit00@gmail.com"),
+              trailing: Icon(Icons.send),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+            ),
+          ],
+        ),
+      ),
+
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+
+        child: Icon(Icons.edit),
+        // elevation: 150.0,
+        mini: false,
+      ),
+    );
   }
 }
