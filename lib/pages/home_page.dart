3b68d4820c9b50bc8c6895fd801854be6cab28bb
Burhan Ahmed <burhan43>
2021-04-29 11:59:59 +0500

    commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:beginner_codelab/bgimg.dart';
 import 'package:beginner_codelab/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,16 +12,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController _nameController= TextEditingController();
-  var txt="Some Text";
+  TextEditingController _nameController = TextEditingController();
+  var txt = "Some Text";
 
+  // ('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
 
+  var url = Uri.https("jsonplaceholder.typicode.com", "/photos");
+  var data;
 
   @override
   void initState() {
     super.initState();
-
+    getData();
   }
+
+  getData() async {
+    var res = await http.get(url);
+
+    print(res.body);
+
+    data = jsonDecode(res.body);
+
+    setState(() {});
+
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,17 +50,26 @@ class _HomePageState extends State<HomePage> {
 
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Card(
-
-              child: ChangeNameWidget(txt: txt, nameController: _nameController),
-            ),
-          ),
-        ),
+        child: data != null
+            ? ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(data[index]["title"]),
+                      subtitle:Text("ID: ${data[index]["id"]}"),
+                      leading: Image.network(data[index]["url"]),
+                    ),
+                  );
+                },
+                itemCount: data.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
 
-      drawer:MyDrawer(),
+      drawer: MyDrawer(),
 
       // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
 
@@ -53,12 +80,9 @@ class _HomePageState extends State<HomePage> {
   FloatingActionButton buildFloatingActionButton() {
     return FloatingActionButton(
       onPressed: () {
-        txt= _nameController.text;
+        txt = _nameController.text;
 
-        setState(() {
-
-        });
-
+        setState(() {});
       },
 
       child: Icon(Icons.refresh),
